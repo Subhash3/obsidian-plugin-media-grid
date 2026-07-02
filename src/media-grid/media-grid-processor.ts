4f8cc678c8d4c703c569a1c9a5bc788b4e60f82d
Subhash Sarangi <subhashsarangi123@gmail.com>
2026-07-02 17:20:56 +0530

    Render flexbox if cols property isn't provided

--- src/media-grid/media-grid-processor.ts
+++ src/media-grid/media-grid-processor.ts
@@ -2,6 +2,7 @@ import type { App, MarkdownPostProcessorContext } from "obsidian";
 import { parseSource } from "./parser/parse-source.js";
 import { createCallout } from "../utils/create-callout.js";
 import { createMediaElement } from "../utils/create-media.js";
+import type { MediaGridConfig } from "../config/plugin-config.js";
 
 export function getMediaGridProcessor(app: App) {
     return function mediaGridProcessor(source: string, container: HTMLElement, ctx: MarkdownPostProcessorContext) {
@@ -17,12 +18,22 @@ export function getMediaGridProcessor(app: App) {
             warningsDiv.append(callOut);
         }
 
-        const grid = container.createDiv({ cls: 'media-grid-container' });
-        grid.id = config.gridContainerId;
-        grid.style.gridTemplateColumns = `repeat(${config.cols}, 1fr)`
-        grid.style.gap = `${config.gap}px`;
+        if(config.cols) {
+            renderGrid(app, container, config);
+        } else {
+            renderFlexbox(app, container, config);
+        }
+    }
+}
 
-        for (let file of config.files) {
+function renderGrid(app: App, container: HTMLElement, config: MediaGridConfig) {
+    const grid = container.createDiv({ cls: 'media-grid-container grid' });
+    grid.id = config.gridContainerId;
+    grid.style.gridTemplateColumns = `repeat(${config.cols}, 1fr)`
+    grid.style.gap = `${config.gap}px`;
+
+    for (const row of config.rows) {
+        for (let file of row) {
             const resourceLink = getResourceLinkFromFileName(app, file.filename);
 
             const gridCell = grid.createDiv({ cls: 'media-grid-cell' });
@@ -30,6 +41,30 @@ export function getMediaGridProcessor(app: App) {
             gridCell.append(mediaEl);
             grid.append(gridCell);
         }
+    }
+}
+
+function renderFlexbox(app: App, container: HTMLElement, config: MediaGridConfig) {
+    const grid = container.createDiv({ cls: 'media-grid-container flex' });
+    grid.id = config.gridContainerId;
+    grid.style.gap = `${config.gap}px`;
+
+    for (const row of config.rows) {
+        const gridRow = grid.createDiv({ cls: 'media-grid-row' })
+        gridRow.style.gap = `${config.gap}px`;
+
+        for (let file of row) {
+            const resourceLink = getResourceLinkFromFileName(app, file.filename);
+
+            const gridCell = grid.createDiv({ cls: 'media-grid-cell' });
+            const mediaEl = createMediaElement(resourceLink, {
+                ...file,
+                size: 0
+            });
+            gridCell.append(mediaEl);
+            gridRow.append(gridCell);
+        }
+        grid.appendChild(gridRow);
     }
 }
 
